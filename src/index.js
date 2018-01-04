a1c9e29761134148e745251d2b430fa9df7b5cce
Matt Tibby <matthew.tibby.contractor@macmillan.com>
2018-01-04 13:07:02 -0600

    file changes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
 import reducers from './reducers';
 
+const API_KEY = 'AIzaSyARiUZmwrx78snfQZMZGzlE4yup57Nufwg';
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+const App = () => {
+  return <div>React simple starter</div>;
+};
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
